f6bf71b1c095a3a7424b086dc69b9413dda91dff
azharprabudi <azharprabui@gmail.com>
2024-12-06 14:41:59 +0700

    revert: the gitlab url in gitconfig for allowing pull the pkg from private repository

--- nix/home.nix
+++ nix/home.nix
@@ -205,10 +205,6 @@ in
         init = {
           defaultBranch = "main";
         };
-
-        "url \"git@gitlab.com:\"" = {
-          insteadOf = https://gitlab.com/;
-        };
       };
     };
 
